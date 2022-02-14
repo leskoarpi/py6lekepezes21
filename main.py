358aa3d1ff97b08a4d4da94cb230b201756d602b
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-02-14 13:57:59 +0000

    szam

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Adott egy függvény. Értelmezési tartománya: [-3; 4] intervallumon az egész számok halmaza. Hozzárendelési szabálya: y = 2x-3.
 A program egy lista formájában tárolja az értelmezési tartomány elemeit. Listaelemek leképezésével állítsa elő, tárolja listában és írja ki az értékkészlet elemeit.
 '''
-erttar = [i for i in range(-3, 4) ]
+erttar = [szam for szam in range(-3, 4) ]
 
-y = [2*i-3 for i in erttar]
+y = [2*szam-3 for szam in erttar]
 print(y)
